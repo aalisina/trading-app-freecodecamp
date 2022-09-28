c7e150be975c949a993b33f98ac04a3e620b9e4e
sinazay123 <sinazay123@gmail.com>
2022-09-28 12:07:57 -0500

    Added check to only set the state when the component is mounted

--- src/Components/StockList.jsx
+++ src/Components/StockList.jsx
@@ -3,7 +3,9 @@ import finnhub from "../Apis/finnhub";
 
 function StockList() {
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+  const [stocks, setStocks] = useState([]);
   useEffect(() => {
+    let isMounted = true;
     const fetchData = async () => {
       try {
         const resp = await finnhub.get("/quote", {
@@ -12,10 +14,15 @@ function StockList() {
           },
         });
         console.log(resp);
+        if (isMounted) {
+          setStocks(resp.data);
+        }
       } catch (error) {}
     };
 
     fetchData();
+    // When the component gets unmounted
+    return () => (isMounted = false);
   }, []);
 
   return <h3>Stock List</h3>;
