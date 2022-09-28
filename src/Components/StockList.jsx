2583f57f00ceda21d25540ae9b8654013cad347e
sinazay123 <sinazay123@gmail.com>
2022-09-28 11:55:00 -0500

    Fetched data for MSFT from the finnhub api

--- src/Components/StockList.jsx
+++ src/Components/StockList.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import finnhub from "../Apis/finnhub";
+
+const TOKEN = process.env.REACT_APP_FINNHUB_API_KEY;
 
 function StockList() {
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await finnhub.get(`/quote?symbol=MSFT&token=${TOKEN}`);
+        console.log(resp);
+      } catch (error) {}
+    };
+
+    fetchData();
+  }, []);
+
   return <h3>Stock List</h3>;
 }
 
