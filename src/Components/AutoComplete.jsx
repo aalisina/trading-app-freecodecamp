6b44d69a2ebba3f65c35bafb499c4ede3631364e
sinazay123 <sinazay123@gmail.com>
2022-09-29 21:32:17 -0500

    Added an input element for the search field

--- src/Components/AutoComplete.jsx
+++ src/Components/AutoComplete.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function AutoComplete() {
-  return <h5>AutoComplete</h5>;
+  return (
+    <div className="w-50 p-5 rounded mx-auto">
+      <div className="form-floating dropdown">
+        <input
+          type="text"
+          style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
+          id="search"
+          className="form-control"
+          placeholder="Search"
+          autoComplete="off"
+        />
+        <label htmlFor="search">Search</label>
+      </div>
+    </div>
+  );
 }
 
 export default AutoComplete;
