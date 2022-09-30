55af4cfa31a173f615b674c8d5beeb7f4be86231
asalizay <sinazay123@gmail.com>
2022-09-30 19:05:47 +0100

    Added 2 functions to add and remove a stock from the watchList

--- src/Context/WatchListContext.jsx
+++ src/Context/WatchListContext.jsx
@@ -4,11 +4,21 @@ export const WatchListContext = createContext();
 
 export const WatchListContextProvider = (props) => {
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+  const addStock = (symbol) => {
+    if (watchList.indexOf(symbol) === -1) {
+      setWatchList([...watchList, symbol]);
+    }
+  };
+
+  const deleteStock = (symbol) => {
+    const modifiedList = watchList.filter((el) => el !== symbol);
+    setWatchList(modifiedList);
+  };
 
   const valuesObj = {
     watchList,
-    // addStock,
-    // deleteStock,
+    addStock,
+    deleteStock,
   };
 
   return (
