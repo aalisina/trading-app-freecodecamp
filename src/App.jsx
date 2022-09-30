c129f3500c2ca8cc085d0f8e23c4caeb7b607469
asalizay <sinazay123@gmail.com>
2022-09-30 15:54:01 +0100

    Wrapped the components of the app within the WatchListProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StockOverview from "./Pages/StockOverview";
 import StockDetail from "./Pages/StockDetail";
+import { WatchListContextProvider } from "./Context/WatchListContext";
 
 function App() {
   return (
     <div className="App">
       <main className="container">
-        <Router>
-          <Routes>
-            <Route path="/" element={<StockOverview />} />
-            <Route path="/detail/:symbol" element={<StockDetail />} />
-          </Routes>
-        </Router>
+        <WatchListContextProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<StockOverview />} />
+              <Route path="/detail/:symbol" element={<StockDetail />} />
+            </Routes>
+          </Router>
+        </WatchListContextProvider>
       </main>
     </div>
   );
