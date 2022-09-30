71fbeba309e7670405464ed7b2f67f6f9e4ae143
sinazay123 <sinazay123@gmail.com>
2022-09-29 22:54:09 -0500

    Added functionality to render a dropdown menu based on the results from the api

--- src/Components/AutoComplete.jsx
+++ src/Components/AutoComplete.jsx
@@ -5,6 +5,21 @@ function AutoComplete() {
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
 
+  const renderDropdown = () => {
+    const dropDownClass = search ? "show" : null;
+    return (
+      <ul className={`dropdown-menu ${dropDownClass}`}>
+        {results.map((stock) => {
+          return (
+            <li className="dropdown-item" key={stock.symbol}>
+              {stock.description} ({stock.symbol})
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
   useEffect(() => {
     let isMounted = true;
     const fetchData = async () => {
@@ -45,16 +60,17 @@ function AutoComplete() {
           onChange={(e) => setSearch(e.target.value)}
         />
         <label htmlFor="search">Search</label>
+        {renderDropdown()}
         {/* When you assign the classname of dropdown-menu, bootstrap automatically gives it a display
         of none, so it won't be visible. If we want to show it, we add the class show to it 
                 <ul className="dropdown-menu show">
         */}
-        <ul className="dropdown-menu">
+        {/* <ul className="dropdown-menu show">
           <li>Stock1</li>
           <li>Stock2</li>
           <li>Stock3</li>
           <li>Stock4</li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
