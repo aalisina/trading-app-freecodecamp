653bf50cd5d54421ea7241f5c518be2314a580eb
sinazay123 <sinazay123@gmail.com>
2022-10-01 11:21:05 -0500

    Left off 7:07

--- src/Pages/StockDetail.jsx
+++ src/Pages/StockDetail.jsx
@@ -1,7 +1,12 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 function StockDetail() {
   const { symbol } = useParams();
+  useEffect(() => {
+    const fetchData = async () => {};
+  }, []);
   return <h2>Stock details {symbol}</h2>;
 }
 
