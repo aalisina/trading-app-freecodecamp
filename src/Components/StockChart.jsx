0879f0e64ebe2bfccb524ecae9f462a4220db082
sinazay123 <sinazay123@gmail.com>
2022-10-05 20:46:40 -0500

    Added options and the series for the chart

--- src/Components/StockChart.jsx
+++ src/Components/StockChart.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import Chart from "react-apexcharts";
 
 function StockChart({ chartData, symbol }) {
   const { day, week, year } = chartData;
-  return <h3>Stock chart</h3>;
+  const options = {
+    title: {
+      text: symbol,
+      align: "center",
+      style: {
+        fontSize: "24px",
+      },
+    },
+    chart: {
+      id: symbol,
+      animations: {
+        speed: 1300,
+      },
+    },
+    xaxis: {
+      type: "datetime",
+    },
+  };
+  const series = [
+    {
+      name: symbol,
+      data: day,
+    },
+  ];
+  return <div></div>;
 }
 
 export default StockChart;
